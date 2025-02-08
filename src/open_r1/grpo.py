f63893942a926c4072d8b350f556e96fa79c160f
baohaoliao <liaobaohao@gmail.com>
2025-02-08 18:46:37 +0100

    src/open_r1/grpo

--- src/open_r1/grpo.py
+++ src/open_r1/grpo.py
@@ -24,9 +24,9 @@ from datasets import load_dataset, load_from_disk
 from transformers import set_seed
 from transformers.trainer_utils import get_last_checkpoint
 
-from open_r1.configs import GRPOConfig
-from open_r1.rewards import accuracy_reward, format_reward, get_cosine_scaled_reward, reasoning_steps_reward
-from open_r1.utils.callbacks import get_callbacks
+from src.open_r1.configs import GRPOConfig
+from src.open_r1.rewards import accuracy_reward, format_reward, get_cosine_scaled_reward, reasoning_steps_reward
+from src.open_r1.utils.callbacks import get_callbacks
 from trl import GRPOTrainer, ModelConfig, ScriptArguments, TrlParser, get_peft_config
 
 
